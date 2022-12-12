89064abb1cc7afd77579c12597a6b8701434c899
Jorge <1833981@alu.murciaeduca.es>
2022-12-12 19:58:17 +0100

    Rutas, css y html

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/logout', function () {
+    return 'Sesión cerrada correctamente';
+});
+
+Route::get('/servicios', function () {
+    return view('servicios');
+});
+
+Route::get('/servicios/show/{id}', function ($id) {
+    return view('servicios.show', array('id' => $id));
+});
+
+Route::get('/servicios/create', function () {
+    return view('servicios.create');
+});
+
+Route::get('/servicios/edit/{id}', function ($id) {
+    return view('servicios.edit', array('id' => $id));
+});
